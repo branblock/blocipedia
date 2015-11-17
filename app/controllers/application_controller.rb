b41adf465ebe6bbd355bcb3d4e212099d0c3acb7
Brandon Block <branblock@gmail.com>
2015-11-17 14:33:34 -0800

    wiki CRUD implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     wikis_path
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 end
