660afddd4e3cb72addecbbd5e3836f8f81a6541d
Brandon Block <branblock@gmail.com>
2015-11-18 14:52:31 -0800

    private wikis implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
 
   def downgrade
-    downgrade_role
-    @user.save!
-      flash[:notice] = "Your account has been downgraded successfully."
-      redirect_to wikis_path
+    current_user.downgrade_role
+    current_user.save!
+
+    @wikis = Wiki.where(private: true, user_id: current_user.id)
+    @wikis.each do |wiki|
+      wiki.set_public
+      wiki.save!
+    end
+
+    flash[:notice] = "Your account has been downgraded successfully."
+    redirect_to wikis_path
   end
 end
