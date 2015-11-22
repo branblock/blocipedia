9c6fe3d092de7e0727b9e688599a22081f56c991
Brandon Block <branblock@gmail.com>
2015-11-21 17:41:39 -0800

    refactoring-and-styling home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,21 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def resource_name
+      :user
+    end
+
+    def resource
+      @resource ||= User.new
+    end
+
+    def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+    end
+
+  helper_method :resource, :resource_name, :devise_mapping
 
   def after_sign_in_path_for(resource)
     wikis_path
